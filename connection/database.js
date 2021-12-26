aafda7eecf105eba8b875dcd1dfd7199b09e3c3f
farawehassan <farawehassan@yahoo.com>
2021-12-26 09:18:28 +0100

    debugging is-auth

--- connection/database.js
+++ connection/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');  
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWPORD}@taiwocluster0.yhpyt.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+console.log(uri)
 const connectDB = async () => {
   await mongoose.connect(uri, {
     useNewUrlParser: true, 
